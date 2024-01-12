39bcd300dec6132429f1a97628633d8831f9718d
praj <praj@gmail.com>
2024-01-12 14:40:18 +0530

    Changes made successfully

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App1.css';
+import Navbar from './Component/Navbar';
+import TextForm from './Component/TextForm';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> {/* //fragment */}
+  <Navbar title="TEXTCONVETER"/>
+  {/* props */}
+<div className="container">
+
+  <TextForm heading="Enter the text to analysis"/>
+</div>
+   </>
   );
 }
 
